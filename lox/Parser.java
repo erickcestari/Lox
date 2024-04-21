60dde3965886fd3a81b6ef7b517fb7e334c427de
Erick Cestari <erickcestari03@gmail.com>
2024-04-21 17:48:12 -0300

    feat: add LoxCallable

--- lox/Parser.java
+++ lox/Parser.java
@@ -45,12 +45,14 @@ class Parser {
   }
 
   private Stmt statement() {
-    if (match(FOR)) return forStatement();
+    if (match(FOR))
+      return forStatement();
     if (match(IF))
       return ifStatement();
     if (match(PRINT))
       return printStatement();
-    if (match(WHILE)) return whileStatement();
+    if (match(WHILE))
+      return whileStatement();
     if (match(LEFT_BRACE))
       return new Stmt.Block(block());
 
@@ -90,7 +92,8 @@ class Parser {
               new Stmt.Expression(increment)));
     }
 
-    if (condition == null) condition = new Expr.Literal(true);
+    if (condition == null)
+      condition = new Expr.Literal(true);
     body = new Stmt.While(condition, body);
 
     if (initializer != null) {
@@ -255,7 +258,38 @@ class Parser {
       return new Expr.Unary(operator, right);
     }
 
-    return primary();
+    return call();
+  }
+
+  private Expr finishCall(Expr callee) {
+    List<Expr> arguments = new ArrayList<>();
+    if (!check(RIGHT_PAREN)) {
+      do {
+        if (arguments.size() >= 255) {
+          error(peek(), "Can't have more than 255 arguments.");
+        }
+        arguments.add(expression());
+      } while (match(COMMA));
+    }
+
+    Token paren = consume(RIGHT_PAREN,
+        "Expect ')' after arguments.");
+
+    return new Expr.Call(callee, paren, arguments);
+  }
+
+  private Expr call() {
+    Expr expr = primary();
+
+    while (true) {
+      if (match(LEFT_PAREN)) {
+        expr = finishCall(expr);
+      } else {
+        break;
+      }
+    }
+
+    return expr;
   }
 
   private Expr primary() {
